8edd3fffa33ef61c2a86093e9327b7d1fc71ba61
trinh van dat <trinhvandat90399@gmail.com>
2020-08-16 15:29:19 +0700

    hibernate: study(initial commit)

---
+++ src/main/java/com/studt/trinhvandat/springboothibernatedemo/SpringBootHibernateDemoApplication.java
@@ -0,0 +1,13 @@
+package com.studt.trinhvandat.springboothibernatedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHibernateDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHibernateDemoApplication.class, args);
+    }
+
+}
